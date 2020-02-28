ee57679a5a1ff2cd971d1b4b6be4a8ee68e2ba78
mcatm <tkcs@pelepop.com>
2020-02-29 00:37:42 +0900

    fix the section for wordpress.

--- wordpress/html/assets/themes/ob/index.php
+++ wordpress/html/assets/themes/ob/index.php
@@ -1,2 +1,25 @@
-<h1><?php bloginfo( 'name' )?></h1>
-<p><?php bloginfo('description')?></p>
+<?php
+
+get_template_part('header');
+
+$q = new WP_Query([
+  'post_type' => 'post',
+]);
+
+?>
+
+<?php if($q->have_posts()):?>
+<h2>投稿記事</h2>
+
+<?php
+
+while($q->have_posts()):
+$q->the_post();
+
+?>
+<div>
+  <h3>タイトル：<?php the_title()?></h3>
+  <h4>サブタイトル：<?php the_field('subtitle')?></h4>
+</div>
+<?php endwhile?>
+<?php endif?>
