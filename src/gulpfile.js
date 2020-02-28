ddb750af77a3376234b195bb05b5bc36cca4e4ed
mcatm <tkcs@pelepop.com>
2020-02-29 03:28:26 +0900

    wip on gulp.

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,8 +1,38 @@
 var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+var rename = require("gulp-rename");
+var ejs = require("gulp-ejs");
+var sass = require('gulp-sass');
+
+gulp.task("css", (done) => {
+  gulp
+    .src("scss/**/*scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./public/css"));
+  done();
+});
+
+gulp.task("ejs", (done) => {
+  gulp
+    .src(["ejs/**/*.ejs", "!" + "ejs/**/_*.ejs"])
+    .pipe(ejs({}, {}, {ext:'.html'}))
+    .pipe(rename({ extname: ".html" }))
+    .pipe(gulp.dest("./public"));
+  done();
+});
 
 gulp.task('hello', gulp.series( function(done) {
   console.log('Hello gulp!');
   done();
 }) );
+
+gulp.task('server', gulp.series( function(done) {
+  gulp.src('public')
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
+  done();
+} ));
 
 gulp.task('default', gulp.task('hello'));
